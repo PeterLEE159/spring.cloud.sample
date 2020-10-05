2a006e3f55df3a70b512ecc71d79be929c54c12a
PeterLEE159 <peterlee13795@gmail.com>
2020-10-06 00:41:59 +0900

    h2

---
+++ cloud-service-h2/src/test/java/com/recom/core/CloudServiceH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recom.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
